585431659a45ff8db3a8e96c662b140cf898614e
Alexander Babak <bbkshow1987@gmail.com>
2022-08-10 18:13:32 +0300

    validate form

--- src/modules/contact/Contact.js
+++ src/modules/contact/Contact.js
@@ -3,15 +3,22 @@ import "./Contact.css";
 import { useRef, useState } from "react";
 import emailjs from "@emailjs/browser";
 import contactImg from "../../img/about-9.jpg";
+import { useForm } from "react-hook-form";
 
 function Contact() {
+  const [done, setDone] = useState(false);
   const formRef = useRef();
 
-  const [done, setDone] = useState(false);
+  const {
+    handleSubmit,
+    register,
+    reset,
+    formState: { errors },
+  } = useForm({
+    mode: "onBlur",
+  });
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-
+  const handleFormSubmit = handleSubmit(() => {
     emailjs
       .sendForm(
         "service_rvqjoe8",
@@ -28,7 +35,8 @@ function Contact() {
           console.log(error.text);
         }
       );
-  };
+    reset();
+  });
 
   return (
     <div className="contact component__space" id="contact">
@@ -43,31 +51,64 @@ function Contact() {
                 <strong>bbkshow1987@gmail.com</strong>
               </p>
             </div>
-            <form className="input__box" ref={formRef} onSubmit={handleSubmit}>
+            <form
+              className="input__box"
+              ref={formRef}
+              onSubmit={handleFormSubmit}
+            >
               <input
-                type="text"
-                name="user_name"
                 className="contact name"
-                placeholder="Ваше имя *"
+                placeholder="Ваше имя"
+                {...register("user_name", {
+                  required: "Name is required",
+                  minLength: 2,
+                })}
               />
+              {errors.user_name && (
+                <p className="warning1">Введите имя длиной от 2-х символов</p>
+              )}
               <input
-                type="text"
-                name="user_email"
                 className="contact email"
-                placeholder="Ваш Email *"
-              />
-              <input
+                placeholder="Ваш Email"
                 type="text"
-                name="user_subject"
+                {...register("user_email", {
+                  required: "Email is required",
+                  pattern:
+                    /^((([0-9A-Za-z]{1}[-0-9A-z\.]{0,30}[0-9A-Za-z]?)|([0-9А-Яа-я]{1}[-0-9А-я\.]{0,30}[0-9А-Яа-я]?))@([-A-Za-z]{1,}\.){1,}[-A-Za-z]{2,})$/,
+                })}
+              />
+              {errors.user_email && (
+                <p className="warning2">Введите корректный email</p>
+              )}
+              <input
                 className="contact subject"
                 placeholder="Тема письма"
+                {...register("user_subject", {
+                  required: "Name is required",
+                  min: 2,
+                  max: 20,
+                })}
               />
+              {errors.user_subject && (
+                <p className="warning3">
+                  Введите тему от 2-х до 20-ти символов
+                </p>
+              )}
               <textarea
                 name="message"
                 id="message"
+                {...register("message", {
+                  required: "Message is required",
+                  min: 1,
+                  max: 100,
+                })}
                 placeholder="Напишите ваше сообщение"
-                style={{ fontFamily: "sans-serif" }}
+                style={{ fontFamily: "sans-serif", resize: "none" }}
               ></textarea>
+              {errors.message && (
+                <p className="warning4">Введите сообщение до 100 символов</p>
+              )}
+
               <button className="btn contact pointer" type="submit">
                 Отправить
               </button>
